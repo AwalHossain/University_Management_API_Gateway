5112150c8a479664d30293ee2adbc12cf320532e
AwalHossain <waqin21@gmail.com>
2023-09-20 12:04:58 +0600

    feat: Add Functionality to create a student ✅

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,14 +1,48 @@
 
 import { Request } from 'express';
 import uploadCloudinary from '../../../helpers/cloudinary';
+import { IGenericResponse } from '../../../interfaces/common';
+import { AuthService } from '../../../shared/axios';
 
 const createStudent = async (req: Request) => {
     const file = req.file;
-    console.log(file);
-
     const uploadImage = await uploadCloudinary(file);
 
-    console.log(uploadImage, 'uploadImage');
+    if (uploadImage) {
+        req.body.profileImage = uploadImage.secure_url;
+    }
+
+    const { academicFaculty, academicDepartment, academicSemester } = req.body.student;
+
+    console.log(academicFaculty, academicDepartment, academicSemester, 'checking faculty departmen semester');
+
+
+    const academicDepartmentResponse = await AuthService.get(`/academic-departments?syncId=${academicDepartment}`)
+
+
+    if (academicDepartmentResponse.data.length > 0 && Array.isArray(academicDepartmentResponse.data)) {
+        req.body.student.academicDepartment = academicDepartmentResponse.data[0]._id;
+    }
+
+    const facultyResponse = await AuthService.get(`/academic-faculties?syncId=${academicFaculty}`)
+
+    if (facultyResponse.data.length > 0 && Array.isArray(facultyResponse.data)) {
+        req.body.student.academicFaculty = facultyResponse.data[0]._id;
+    }
+
+    const semesterResponse = await AuthService.get(`/academic-semesters?syncId=${academicSemester}`)
+    if (semesterResponse.data.length > 0 && Array.isArray(semesterResponse.data)) {
+        req.body.student.academicSemester = semesterResponse.data[0]._id;
+    }
+
+    const response: IGenericResponse = await AuthService.post('/users/create-student', req.body, {
+        headers: {
+            Authorization: req.headers.authorization
+        }
+    });
+
+    console.log(response, 'response');
+    return response;
 }
 
 
